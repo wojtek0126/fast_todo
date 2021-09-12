0fbd2523cb02d98dfba50772986160b0b5308eac
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-09-13 01:20:51 +0200

    lil content fix

--- eazzytodo/src/content/content.tsx
+++ eazzytodo/src/content/content.tsx
@@ -8,7 +8,7 @@ I can do faster than that. Loads of time available now :) So far it is supportin
 
 export const txtTitleBigEng: string = "This is my personal todo list app made with React.js and several of it's libraries.\
  Feel free to sign in, assign Yourself some task and of course try to make it done. This app is evolving so next time around\
-it will most likely look and work different. Also the code and file structure\
+ it will most likely look and work different. Also the code and file structure\
  will be improved. Right now this todo list is free for anyone to test, maximum number of tasks is currently set to 50...have fun! "
 
 export const txtAddTaskBtnEng: string = "Add Task";
