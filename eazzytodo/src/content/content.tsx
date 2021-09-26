3cae7e384b3d8271c00dc62c6407643954ef9f0d
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-09-27 00:03:48 +0200

    updated content title

--- eazzytodo/src/content/content.tsx
+++ eazzytodo/src/content/content.tsx
@@ -7,9 +7,9 @@ export const txtSearchInputEng: string = "Search for task..";
 export const txtTitleSmallEng: string = "Welcome to my personal Todo list. Happy list. With this thing I am honing my React skills.";
 
 export const txtTitleBigEng: string = "This is my personal todo list app made with React.js and several of it's libraries.\
- Feel free to sign in, assign Yourself some task and of course try to make it done. This app is evolving so next time around\
- it will most likely look and work different. Also the code and file structure\
- will be improved. Right now this todo list is free for anyone to test, maximum number of tasks is currently set to 50...have fun! "
+ Feel free to sign in, assign Yourself some task and of course try to make it done. It accepts any email, mock email recommended,\
+  because I test user email display next to tasks and Your email will be seen by all testers or users. Task space is now open to anyone.\
+  Maximum number of tasks is currently set to 50...have fun! "
 
 export const txtAddTaskBtnEng: string = "Add Task";
 
