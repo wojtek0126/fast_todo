a25559fa034c41bcd8522a725f9ee0b6a17e0ba1
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-10-02 15:27:59 +0200

    little upgrades

--- eazzytodo/src/content/content.tsx
+++ eazzytodo/src/content/content.tsx
@@ -4,7 +4,7 @@ export const txtSignOutEng: string = "Sign out";
 
 export const txtSearchInputEng: string = "Search for task..";
 
-export const txtTitleSmallEng: string = "Welcome to eazzy Todo list. Set a goal and make it done. Multipurpose usage. Have a happy todoing!";
+export const txtTitleSmallEng: string = "Welcome to ever evolving Eazzy Todo List. Set Yourself a goal and make it done. Multipurpose usage. Have a happy todoing!";
 
 export const txtTitleBigEng: string = "This app is currently in test mode. mock email recommended.\
  Feel free to sign in, assign Yourself some task, modify it at will, and of course try to make it completed.\
